c8e92e76b66a5b6f1dd031e92706c194221748ba
yooniversal <iteratively@naver.com>
2023-07-27 21:15:09 +0900

    feat: docker compose 실행 로직 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,3 +20,8 @@ nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+
+# docker-compose 실행
+echo "$TIME_NOW > Docker Compose Up" >> $DEPLOY_LOG
+cd $PROJECT_ROOT
+docker-compose up -d
