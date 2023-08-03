22230ca4287099eb7766316b0c987b12287b5ceb
yooniversal <iteratively@naver.com>
2023-08-04 03:41:20 +0900

    fix: 생성자 적절한 파라미터 설정

--- src/main/java/yeolJyeongKong/mall/service/FavoriteService.java
+++ src/main/java/yeolJyeongKong/mall/service/FavoriteService.java
@@ -52,7 +52,8 @@ public class FavoriteService {
                 productDtos.add(new MallRankProductDto(product.getId(), product.getImage()));
             }
 
-            result.add(new MallDto(mall.getName(), mall.getUrl(), mall.getImage(), mall.getDescription(), productDtos));
+            result.add(new MallDto(mall.getId(), mall.getName(), mall.getUrl(), mall.getImage(),
+                    mall.getDescription(), 0, productDtos));
         }
 
         return result;
