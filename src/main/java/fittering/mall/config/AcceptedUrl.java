f776faa21f12e99e6aaf69b768093aac42e298c2
yooniversal <iteratively@naver.com>
2023-08-19 03:18:21 +0900

    feat: 권한 제한없는 URL 모음 클래스 정의

---
+++ src/main/java/fittering/mall/config/AcceptedUrl.java
@@ -0,0 +1,14 @@
+package fittering.mall.config;
+
+import java.util.List;
+
+public class AcceptedUrl {
+    public final static List<String> ACCEPTED_URL_LIST = List.of(
+            "/login", "/signup", "/error",
+            "/api/v1/login", "/api/v1/signup",
+            "/swagger-ui.html", "/swagger-ui/index.html", "/swagger-ui/index.css", "/swagger-ui/swagger-ui.css",
+            "/swagger-ui/swagger-ui-bundle.js", "/swagger-ui/swagger-ui-standalone-preset.js", "/swagger-ui/swagger-initializer.js",
+            "/swagger-ui/favicon-32x32.png", "/swagger-ui/favicon-16x16.png", "/api-docs/json/swagger-config",
+            "/api-docs/json", "/actuator/prometheus"
+    );
+}
