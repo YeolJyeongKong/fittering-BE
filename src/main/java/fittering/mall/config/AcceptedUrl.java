14f05470fa7f49da05cbd1347aa3f0d2c2dfe7d5
yooniversal <iteratively@naver.com>
2023-08-20 16:45:33 +0900

    fix: 카카오 API URI 접근 허용 오타 수정

--- src/main/java/fittering/mall/config/AcceptedUrl.java
+++ src/main/java/fittering/mall/config/AcceptedUrl.java
@@ -10,6 +10,6 @@ public class AcceptedUrl {
             "/swagger-ui/swagger-ui-bundle.js", "/swagger-ui/swagger-ui-standalone-preset.js", "/swagger-ui/swagger-initializer.js",
             "/swagger-ui/favicon-32x32.png", "/swagger-ui/favicon-16x16.png", "/api-docs/json/swagger-config",
             "/api-docs/json", "/actuator/prometheus",
-            "/login/oauth/apple", "/login/oauth/google", "login/oauth/kakao"
+            "/login/oauth/apple", "/login/oauth/google", "/login/oauth/kakao"
     );
 }
