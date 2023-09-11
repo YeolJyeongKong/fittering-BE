ef6d63da4ec187535da873aa4218491045a8291f
yooniversal <iteratively@naver.com>
2023-09-11 15:29:51 +0900

    feat: 상품 상세조회 API 응답 서브 카테고리 추가

--- src/main/java/fittering/mall/domain/dto/controller/response/ResponseBottomDto.java
+++ src/main/java/fittering/mall/domain/dto/controller/response/ResponseBottomDto.java
@@ -22,6 +22,7 @@ public class ResponseBottomDto {
     private String mallImage;
     private String origin;
     private String category;
+    private String subCategory;
     private Integer view;
     private String popularGender;
     private Integer popularAgeRange;
