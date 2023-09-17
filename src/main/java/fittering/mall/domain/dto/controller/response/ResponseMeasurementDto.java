2925cfb42b608ed9188fae038b89cdc0442ff43f
yooniversal <iteratively@naver.com>
2023-09-17 20:10:12 +0900

    fix: 체형 분석 응답 DTO 필드 타입을 Double로 수정

--- src/main/java/fittering/mall/domain/dto/controller/response/ResponseMeasurementDto.java
+++ src/main/java/fittering/mall/domain/dto/controller/response/ResponseMeasurementDto.java
@@ -10,13 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseMeasurementDto {
-    private Integer height;
-    private Integer weight;
-    private Integer arm;
-    private Integer leg;
-    private Integer shoulder;
-    private Integer waist;
-    private Integer chest;
-    private Integer thigh;
-    private Integer hip;
+    private Double height;
+    private Double weight;
+    private Double arm;
+    private Double leg;
+    private Double shoulder;
+    private Double waist;
+    private Double chest;
+    private Double thigh;
+    private Double hip;
 }
