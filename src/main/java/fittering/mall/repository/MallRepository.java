f4b0aa42015a60a0b93ea2ab55bd1ed3580e4efc
yooniversal <iteratively@naver.com>
2023-09-17 16:22:18 +0900

    fix: 쇼핑몰 이름 기반 조회 Lock 설정

--- src/main/java/fittering/mall/repository/MallRepository.java
+++ src/main/java/fittering/mall/repository/MallRepository.java
@@ -1,12 +1,16 @@
 package fittering.mall.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import fittering.mall.domain.entity.Mall;
 import fittering.mall.repository.querydsl.MallRepositoryCustom;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface MallRepository extends JpaRepository<Mall, Long>, MallRepositoryCustom {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @EntityGraph(attributePaths = "favorites")
     Optional<Mall> findByName(String name);
 }
