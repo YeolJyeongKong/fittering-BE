db0442e9184734e82531e2c7ff698e4cac72ac63
yooniversal <iteratively@naver.com>
2023-09-18 17:56:31 +0900

    feat: 쇼핑몰 이름 및 ID 리스트 API DTO 정의

---
+++ src/main/java/fittering/mall/domain/dto/service/MallNameAndIdDto.java
@@ -0,0 +1,20 @@
+package fittering.mall.domain.dto.service;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+public class MallNameAndIdDto {
+    private Long id;
+    private String name;
+
+    @QueryProjection
+    public MallNameAndIdDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
