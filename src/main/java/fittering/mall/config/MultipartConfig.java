e7d3d0fad97ed5485e07e126f725c9f34a7f115d
yooniversal <iteratively@naver.com>
2023-09-20 16:45:45 +0900

    feat: 파일 업로드 설정 클래스 등록

---
+++ src/main/java/fittering/mall/config/MultipartConfig.java
@@ -0,0 +1,29 @@
+package fittering.mall.config;
+
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+@Configuration
+public class MultipartConfig {
+
+    private final static long MAX_UPLOAD_SIZE = 10485760;
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
+        return multipartResolver;
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxRequestSize(DataSize.ofBytes(MAX_UPLOAD_SIZE));
+        factory.setMaxFileSize(DataSize.ofBytes(MAX_UPLOAD_SIZE));
+        return factory.createMultipartConfig();
+    }
+}
