feeff8740fea0223fedbe066af2f78662500fe10
yooniversal <iteratively@naver.com>
2023-09-25 02:13:17 +0900

    refactor: kafka bootstrap server 상수 정의

--- src/main/java/fittering/mall/config/kafka/KafkaTemplateConfig.java
+++ src/main/java/fittering/mall/config/kafka/KafkaTemplateConfig.java
@@ -14,11 +14,13 @@ import java.util.Map;
 @Configuration
 public class KafkaTemplateConfig {
 
+    private static final String BOOTSTRAP_SERVER = "localhost:9092";
+
     @Bean
     public KafkaTemplate<String, String> customKafkaTemplate() {
 
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.ACKS_CONFIG, "all");
