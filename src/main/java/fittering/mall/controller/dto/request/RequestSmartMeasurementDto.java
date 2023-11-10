3c7ca3b4b57d124c4946c7da6ee7e5c61988a8f0
yooniversal <iteratively@naver.com>
2023-11-11 05:31:08 +0900

    fix: 스마트 분석 체형 DTO validation 수정

--- src/main/java/fittering/mall/controller/dto/request/RequestSmartMeasurementDto.java
+++ src/main/java/fittering/mall/controller/dto/request/RequestSmartMeasurementDto.java
@@ -1,5 +1,6 @@
 package fittering.mall.controller.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -7,14 +8,14 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestSmartMeasurementDto {
-    @NonNull
+    @NotNull(message = "정면 사진은 필수입니다.")
     private String front;
-    @NonNull
+    @NotNull(message = "측면 사진은 필수입니다.")
     private String side;
-    @NonNull
+    @NotNull(message = "키 정보는 필수입니다.")
     private Double height;
-    @NonNull
+    @NotNull(message = "몸무게 정보는 필수입니다.")
     private Double weight;
-    @NonNull
+    @NotNull(message = "성별 정보는 필수입니다.")
     private String sex;
 }
