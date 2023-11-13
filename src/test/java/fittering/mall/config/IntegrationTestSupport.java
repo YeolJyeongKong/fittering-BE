b70ee86db9db62cea812df9c31c3135098e5f6c8
yooniversal <iteratively@naver.com>
2023-11-13 16:51:12 +0900

    feat: 통합 테스트 설정 정의

---
+++ src/test/java/fittering/mall/config/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package fittering.mall.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+}
